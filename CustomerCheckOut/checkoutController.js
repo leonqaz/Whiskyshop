429fd5610c65148009b3305bbac38a99cb551d30
Leon Oestergaard <leon@qaz.dk>
2015-02-18 19:15:15 +0100

    missing files and folders
    Former-commit-id: 13cc56b0c3d2e4e4383cee9d5cc557c0eb08bde8

---
+++ CustomerCheckOut/checkoutController.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+    angular.module("Main.checkoutController", []).controller("checkoutController", checkoutController);
+    function checkoutController($scope, basketService) {
+       
+       
+
+        $scope.checkout = function () {
+            var basket = basketService.getBasket();
+            console.log(basket);
+            console.log($scope.customer);
+        }
+
+      
+    }
+})();

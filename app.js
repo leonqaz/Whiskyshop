d1640df55a1355cfac5ab5590a7e9dcb83ee465c
unknown <leo@EM01257.em.dk>
2015-02-18 18:46:27 +0100

    First commit
    Former-commit-id: bc81fb84cfe8baa3f30b460fbd0c020329ad4cab

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+    angular.module("Main", ["ngRoute", "Main.productsController", "Main.basketController", "Main.productController", "Main.checkoutController"]).
+    config(function ($routeProvider) {
+        $routeProvider
+        .when('/product/:id', {
+            templateUrl: '/product/product.html',
+            controller: 'productController'
+        }).
+        when('/', {
+            templateUrl: '/products/Products.html',
+            controller: 'productsController'
+        }).
+            when('/checkout/', {
+                templateUrl: '/CustomerCheckOut/checkOut.html',
+                controller: 'basketController'
+            }).
+        otherwise({ redirectTo: '/' });
+
+    })
+    
+})();

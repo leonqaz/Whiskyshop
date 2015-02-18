c4f4ca0a94c2262011e0c90d0e2c0c0703c52588
Leon Oestergaard <leon@qaz.dk>
2015-02-18 19:02:11 +0100

    Test commit

---
+++ Basket/basketController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+    angular.module("Main.basketController", []).controller("basketController", basketController);
+    function basketController($scope, $rootScope,basketService) {
+        $scope.displayContent = false;
+        $scope.basket = basketService.getBasket();
+
+        $scope.showHideContent = function () {
+            console.log($scope.displayContent);
+            $scope.displayContent = !$scope.displayContent;
+        }
+
+        $scope.updateBasketItem = function (item)
+        {
+            basketService.updateBasketItem(item);
+            $rootScope.basketNoOfProducts = basketService.getNoOfProducts();
+            $rootScope.basketPrice = basketService.getTotalPrice();
+        }
+    }
+})();
